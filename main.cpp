fe6a874d6e8df8d355a288baf6cc8ca63c4d5c93
psavchuk <savchukdev@gmail.com>
2020-06-16 19:18:09 +0300

    Context struct doesn't make sense anymore. Remove it.

--- main.cpp
+++ main.cpp
@@ -8,35 +8,19 @@
 #include <poll.h>
 #include <time.h>
 
-#define UNUSED(a) a
-
-// TODO: Come up with a more meaningful name
-typedef struct Context {
-  int fire;
-  char work_dir[PATH_MAX];
-  char tags_file[PATH_MAX];
-} Context;
-
-Context context = {0};
-
-static void handle_events(int fd, int wd, Context context, char *command);
+static void handle_events(int fd, int wd, char *command);
 
 int main(int argc, char **argv) {
-  if (getcwd(context.work_dir, PATH_MAX - 1) == NULL) {
+  char work_dir[PATH_MAX];
+  if (getcwd(work_dir, PATH_MAX - 1) == NULL) {
     perror("Couldn't determine current directory\n");
     exit(EXIT_FAILURE);
   }
   
-  strcpy(context.tags_file, context.work_dir);
-  strcat(context.tags_file, "/tags");
-
   if (argc <= 1) {
     perror("Command required\n");
     exit(EXIT_FAILURE);
   }
- 
-  // fire on startup
-  context.fire = 1;
 
   int inotify_descriptor = inotify_init1(IN_NONBLOCK);
   if (inotify_descriptor == -1) {
@@ -44,9 +28,9 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
-  int watch_descriptor = inotify_add_watch(inotify_descriptor, context.work_dir, IN_CREATE | IN_MODIFY | IN_DELETE);
+  int watch_descriptor = inotify_add_watch(inotify_descriptor, work_dir, IN_CREATE | IN_MODIFY | IN_DELETE);
   if (watch_descriptor == -1) {
-    fprintf(stderr, "Cannot watch %s. Exiting.\n", context.work_dir);
+    fprintf(stderr, "Cannot watch %s. Exiting.\n", work_dir);
     exit(EXIT_FAILURE);
   }
   
@@ -64,7 +48,7 @@ int main(int argc, char **argv) {
 
     if (polls_count > 0) {
       if (poll_fd.revents & POLLIN) {
-        handle_events(inotify_descriptor, watch_descriptor, context, argv[1]);
+        handle_events(inotify_descriptor, watch_descriptor, argv[1]);
       }
     }
 
@@ -73,12 +57,13 @@ int main(int argc, char **argv) {
   return 0;
 }
 
-static void handle_events(int fd, int wd, Context context, char *command) {
+static void handle_events(int fd, int wd, char *command) {
   char buf[4096] __attribute__ ((aligned(__alignof__(struct inotify_event))));
   const struct inotify_event *event;
   ssize_t length;
   char *ptr;
   time_t last_fire = time(0);
+  short fire = 0;
 
   while(1) {
     length = read(fd, buf, sizeof(buf));
@@ -95,12 +80,12 @@ static void handle_events(int fd, int wd, Context context, char *command) {
       if (event->mask & IN_ISDIR) continue;
 
       // if (event->len) printf("%s\n", event->name); TODO: handle exceptions
-      context.fire = 1;
+      fire = 1;
     }
 
-    if (context.fire && time(0) - last_fire > 1) {
+    if (fire && time(0) - last_fire > 1) {
       system(command);
-      context.fire = 0;
+      fire = 0;
       last_fire = time(0);
     }
   }
